3edfb76b27e685b11744a00d88cd6c27f6b261d2
matt berube <mttbrb@gmail.com>
2016-12-30 19:29:09 -0500

    evaluation part 1.1

--- src/Main.hs
+++ src/Main.hs
@@ -77,7 +77,24 @@ parseExpr = parseAtom
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No Match: " ++ show err
-    Right val -> "Found value"
+    Right val -> "Found " ++ show val
+
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+
+instance Show LispVal where show = showVal
+
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
 
 
 main :: IO ()
